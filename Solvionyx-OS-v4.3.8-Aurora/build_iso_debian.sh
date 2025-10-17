1aa84462c6e8e2017906a8ddc6a429dff89dde96
Solviony <deve@solviony.com>
2025-10-16 23:47:54 -0500

    Add Solvionyx OS Aurora v4.3.8 base builder

---
+++ Solvionyx-OS-v4.3.8-Aurora/build_iso_debian.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo ">>> Solvionyx OS Aurora AutoBuilder (v4.3.8)"
+echo ">>> This is a placeholder script to confirm setup."
+echo ">>> Later versions will include full build logic."
